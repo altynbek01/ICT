c2b598351247b71d0603e13f58946ad91fd63415
Altynbek <altynbek01@gmail.com>
2021-02-06 15:32:06 +0600

    'init'

---
+++ Lab_0/lab0_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lab0_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string DefangIPaddr = Console.ReadLine();
+            DefangIPaddr = DefangIPaddr.Replace(".", "[.]");
+            Console.WriteLine(DefangIPaddr);
+
+        }
+    }
+}
